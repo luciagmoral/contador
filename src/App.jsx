41439cea5b97917186003027b084d5ee4b28153c
luciagmoral <146716628+luciagmoral@users.noreply.github.com>
2023-10-18 16:56:07 +0200

    he hecho un contador

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+import Button from './components/button/Button';
+
 const App = () => {
-	return <h1>Núcleo de la aplicación</h1>;
+	const [number, setNumber] = useState(0);
+
+	return (
+		<>
+			<h1>{number}</h1>
+			<Button text='+1' action={() => increment(number, setNumber)} />
+			<Button text='RESET' action={() => reset(number, setNumber)} />
+			<Button text='-1' action={() => reduce(number, setNumber)} />
+
+			{/* <h1>{number}</h1>
+			<button onClick={() => increment(number, setNumber)}>+1</button> */}
+		</>
+	);
 };
 
+const increment = (number, setNumber) => {
+	setNumber(number + 1);
+};
+const reduce = (number, setNumber) => {
+	setNumber(number - 1);
+};
+const reset = (number, setNumber) => {
+	setNumber(0);
+};
 export default App;
